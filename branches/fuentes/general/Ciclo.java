bfda42c27ae3b8109661341b20ed429d0b0ca05e
eze1981 <eze1981@51f2afe3-fa20-0410-ba02-f1e7032c7269>
2006-11-05 16:13:23 +0000

    Ciclo bastante mas completo, con calculo de FPS y UPS.

--- branches/fuentes/general/Ciclo.java
+++ branches/fuentes/general/Ciclo.java
@@ -2,6 +2,13 @@ package general;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
 
@@ -15,17 +22,51 @@ public class Ciclo extends JPanel implements Runnable {
   private Thread animator;                     // for the animation
   private volatile boolean running = false;    // stops the animation  
   private volatile boolean gameOver = false;   // for game termination
-
+  
+  // global variables for off-screen rendering
+  private Graphics dbg;
+  private Image dbImage = null;
 
   public void GamePanel() {
-	setBackground(Color.white);                // white background
-    setPreferredSize( new Dimension(PWIDTH, PHEIGHT));
-    //
+    setBackground(Color.white);
+    setPreferredSize(new Dimension(PWIDTH, PHEIGHT));
+
+    setFocusable(true);
+    requestFocus();    // JPanel now receives key events
+    readyForTermination();
+
     // create game components
     // ...
+
+    // listen for mouse presses
+    addMouseListener(new MouseAdapter() {
+	  public void mousePressed(MouseEvent e) 
+	  {testPress(e.getX( ), e.getY( )); }
+    });
   }
 
-
+  private void readyForTermination() {
+    addKeyListener( new KeyAdapter(){
+      // listen for esc, q, end, ctrl-c
+      public void keyPressed(KeyEvent e){ 
+    	int keyCode = e.getKeyCode( );
+        if ((keyCode == KeyEvent.VK_ESCAPE) ||
+            (keyCode == KeyEvent.VK_Q) ||
+            (keyCode == KeyEvent.VK_END) ||
+            ((keyCode == KeyEvent.VK_C) && e.isControlDown( )) ) {
+          running = false;
+        }
+       }
+     });
+  }
+  
+  // is (x,y) important to the game?
+  private void testPress(int x, int y) {
+    if (!gameOver) {
+      // do something
+    }
+  }
+  
   /* Wait for the JPanel to be added to the
   JFrame/JApplet before starting. */
   public void addNotify() {
@@ -61,9 +102,134 @@ public class Ciclo extends JPanel implements Runnable {
         Thread.sleep(20);  // sleep a bit
       }
       catch(final InterruptedException ex){}
-    }    
-    //  so enclosing JFrame/JApplet exits
+    }
     System.exit(0); 
+  }
+  
+  /* Active Rendering */
+  public void run2(){
+    running = true;
+    while(running) {
+      gameUpdate();
+      gameRender();
+      paintScreen();
+
+      try {
+        Thread.sleep(20);  // sleep a bit
+      }
+      catch(InterruptedException ex){}
+    }
+    System.exit(0);
+  }
+  
+  public void run3() {
+  /* Repeatedly: update, render, sleep so loop takes close
+     to period ms */
+  
+    long beforeTime, timeDiff, sleepTime;
+    beforeTime = System.currentTimeMillis();
+
+    running = true;
+    while(running) {
+      gameUpdate();
+      gameRender();
+      paintScreen();
+
+      timeDiff = System.currentTimeMillis( ) - beforeTime;
+      sleepTime = period - timeDiff;   // time left in this loop
+
+      if (sleepTime <= 0)  // update/render took longer than period
+        sleepTime = 5;    // sleep a bit anyway
+
+      try {
+        Thread.sleep(sleepTime);  // in ms
+      }
+      catch(InterruptedException ex){}
+
+      beforeTime = System.currentTimeMillis( );
+    }
+
+    System.exit(0);
+  }
+  
+  
+  private static int MAX_FRAME_SKIPS = 5;
+  // no. of frames that can be skipped in any one animation loop
+  // i.e the games state is updated but not rendered
+
+  private static final int NO_DELAYS_PER_YIELD = 16;
+  /* Number of frames with a delay of 0 ms before the
+     animation thread yields to other running threads. */
+
+
+  /* Repeatedly update, render, sleep so loop takes close
+  to period nsecs. Sleep inaccuracies are handled.
+  The timing calculation use the Java 3D timer.
+*/
+  public void run4() {
+    long beforeTime, afterTime, timeDiff, sleepTime;
+    long overSleepTime = 0L;
+    int noDelays = 0;
+    long excess = 0L;
+
+    beforeTime = System.nanoTime();
+
+    running = true;
+    while(running) {
+      gameUpdate();
+      gameRender();
+      paintScreen();
+
+      afterTime = System.nanoTime();
+      timeDiff = afterTime - beforeTime;
+      sleepTime = (period - timeDiff) - overSleepTime;
+
+      if (sleepTime > 0) {   // some time left in this cycle
+        try {
+          Thread.sleep(sleepTime/1000000L);  // nano -> ms
+        }
+        catch(InterruptedException ex){}
+        overSleepTime =
+              (System.nanoTime() - afterTime) - sleepTime;
+      }
+      else {    // sleepTime <= 0; frame took longer than the period
+    	excess -= sleepTime;  // store excess time value
+    	overSleepTime = 0L;
+
+        if (++noDelays >= NO_DELAYS_PER_YIELD) {
+          Thread.yield();   // give another thread a chance to run
+          noDelays = 0;
+        }
+      }
+      beforeTime = System.nanoTime();
+      
+      /* If frame animation is taking too long, update the game state
+      without rendering it, to get the updates/sec nearer to
+      the required FPS. */
+      int skips = 0;
+	  while((excess > period) && (skips < MAX_FRAME_SKIPS)) {
+	    excess -= period;
+	    gameUpdate( );      // update state but don't render
+	    skips++;
+	  }
+    }
+
+    System.exit(0);
+  }
+  
+  
+  //actively render the buffer image to the screen
+  private void paintScreen(){
+    Graphics g;
+    try {
+      g = this.getGraphics( );  // get the panel's graphic context
+      if ((g != null) && (dbImage != null))
+        g.drawImage(dbImage, 0, 0, null);
+      Toolkit.getDefaultToolkit( ).sync( );  // sync the display on some systems
+      g.dispose( );
+    }
+    catch (Exception e)
+    { System.out.println("Graphics context error: " + e);  }
   }
 
 
@@ -71,12 +237,44 @@ public class Ciclo extends JPanel implements Runnable {
     if (!gameOver){
     	// update game state ...
 	}
+  }  
+  
+  /* draw the current frame to an image buffer */
+  private void gameRender() {
+	// create the buffer
+    if (dbImage == null){  
+    	
+      dbImage = createImage(PWIDTH, PHEIGHT);
+      if (dbImage == null) {
+        System.out.println("dbImage is null");
+        return;
+      } else
+        dbg = dbImage.getGraphics( );
+    }
+
+    // clear the background
+    dbg.setColor(Color.white);
+    dbg.fillRect (0, 0, PWIDTH, PHEIGHT);
+
+    // draw game elements
+    // ...
+
+    if (gameOver)
+      gameOverMessage(dbg);
   }
   
-  
-  private void gameRender() {	  
-		
+  public void paintComponent(Graphics g)
+  {
+    super.paintComponent(g);
+    if (dbImage != null)
+      g.drawImage(dbImage, 0, 0, null);
   }
+
+  private void gameOverMessage(Graphics g) { 
+	// code to calculate x and y...
+    g.drawString(msg, x, y);
+  } 
+
   
-  // more methods, explained later...
+  
 }
