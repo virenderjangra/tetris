40727488717f3aef8afc7c862f9409af414e85a2
eze1981 <eze1981@51f2afe3-fa20-0410-ba02-f1e7032c7269>
2006-11-06 00:06:12 +0000

    Ciclo casi completo y casi compilando. Se agrega la clase Juego para generalizar a la clase Tetris.

--- general/Ciclo.java
+++ general/Ciclo.java
@@ -2,6 +2,8 @@ package general;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
@@ -22,13 +24,22 @@ public class Ciclo extends JPanel implements Runnable {
   private Thread animator;                     // for the animation
   private volatile boolean running = false;    // stops the animation  
   private volatile boolean gameOver = false;   // for game termination
-  
+  private volatile boolean isPaused = false;   // for game pause
+    
   // global variables for off-screen rendering
   private Graphics dbg;
   private Image dbImage = null;
-
-  public void GamePanel() {
-    setBackground(Color.white);
+  private long period;
+  private double fpsStore[];
+  private Font font;
+  private FontMetrics metrics;
+  private Juego juego;
+  private String gameOverString = "Perdiste looser!!!!!";
+    
+  
+  public Ciclo(Juego j, long period) {
+    this.juego = j;
+	setBackground(Color.white);
     setPreferredSize(new Dimension(PWIDTH, PHEIGHT));
 
     setFocusable(true);
@@ -43,6 +54,19 @@ public class Ciclo extends JPanel implements Runnable {
 	  public void mousePressed(MouseEvent e) 
 	  {testPress(e.getX( ), e.getY( )); }
     });
+    
+    //  set up message font
+    font = new Font("SansSerif", Font.BOLD, 24);
+    metrics = this.getFontMetrics(font);    
+    
+    
+    // initialise timing elements
+    fpsStore = new double[NUM_FPS];
+    upsStore = new double[NUM_FPS];
+    for (int i=0; i < NUM_FPS; i++) {
+      fpsStore[i] = 0.0;
+      upsStore[i] = 0.0;
+    }
   }
 
   private void readyForTermination() {
@@ -84,14 +108,9 @@ public class Ciclo extends JPanel implements Runnable {
     }
   }
   
-  
-  public void stopGame() {  
-	running = false;   
-  }
-
-
+    
   /* Repeatedly update, render, sleep */
-  public void run() {
+  public void run1() {
     running = true;
     while(running) {
       gameUpdate();
@@ -166,13 +185,15 @@ public class Ciclo extends JPanel implements Runnable {
   to period nsecs. Sleep inaccuracies are handled.
   The timing calculation use the Java 3D timer.
 */
-  public void run4() {
+  public void run() {
     long beforeTime, afterTime, timeDiff, sleepTime;
     long overSleepTime = 0L;
     int noDelays = 0;
     long excess = 0L;
 
-    beforeTime = System.nanoTime();
+    gameStartTime = System.nanoTime();    
+    prevStatsTime = gameStartTime;
+    beforeTime = gameStartTime;
 
     running = true;
     while(running) {
@@ -212,11 +233,110 @@ public class Ciclo extends JPanel implements Runnable {
 	    gameUpdate( );      // update state but don't render
 	    skips++;
 	  }
+	  
+	  framesSkipped += skips;
+      storeStats();
     }
-
+    
+    printStats();
     System.exit(0);
   }
   
+  private void printStats() {
+    System.out.println("Frame Count/Loss: " + frameCount +
+                               " / " + totalFramesSkipped);
+    System.out.println("Average FPS: " + df.format(averageFPS));
+    System.out.println("Average UPS: " + df.format(averageUPS));
+    System.out.println("Time Spent: " + timeSpentInGame + " secs");    
+  }
+  
+  //used for gathering statistics
+  // many, see "Swing Timer Animation" section, chapter 2
+  private long gameStartTime;
+  private long prevStatsTime;
+  private int timeSpentInGame = 0;    // in seconds
+	
+  private long framesSkipped = 0L;
+  private long totalFramesSkipped = 0L;
+  private double upsStore[];
+  private double averageUPS = 0.0;
+  
+  private static long MAX_STATS_INTERVAL = 1000L;
+  // record stats every 1 second (roughly)
+
+  //used for gathering statistics
+  private long statsInterval = 0L;    // in ms
+  private static int NUM_FPS = 10;
+  private long frameCount = 0;
+  private long totalElapsedTime = 0L;
+  private int statsCount;
+  private double averageFPS = 0.0;
+
+
+	
+	
+  private void storeStats() {
+	frameCount++;
+	statsInterval += period;
+	
+	if (statsInterval >= MAX_STATS_INTERVAL) {
+	  long timeNow = System.nanoTime();
+	  timeSpentInGame = (int) ((timeNow - gameStartTime)/1000000000L);  // ns-->secs
+	  this.juego.setTimeSpent(timeSpentInGame);
+	
+	  long realElapsedTime = timeNow - prevStatsTime;
+	       // time since last stats collection
+	  totalElapsedTime += realElapsedTime;
+	
+	  double timingError = (double)
+	       (realElapsedTime-statsInterval) / statsInterval)*100.0;
+	
+	  totalFramesSkipped += framesSkipped;
+	
+	  double actualFPS = 0;     // calculate the latest FPS and UPS
+	  double actualUPS = 0;
+	  if (totalElapsedTime > 0) {
+	    actualFPS = (((double)frameCount / totalElapsedTime) *
+	                                             1000000000L);
+	    actualUPS = (((double)(frameCount + totalFramesSkipped) /
+	                 totalElapsedTime) * 1000000000L);
+	  }
+	
+	  // store the latest FPS and UPS
+	  fpsStore[ (int)statsCount%NUM_FPS ] = actualFPS;
+	  upsStore[ (int)statsCount%NUM_FPS ] = actualUPS;
+	  statsCount = statsCount+1;
+	
+	  double totalFPS = 0.0;     // total the stored FPSs and UPSs
+	  double totalUPS = 0.0;
+	  for (int i=0; i < NUM_FPS; i++) {
+	    totalFPS += fpsStore[i];
+	    totalUPS += upsStore[i];
+	  }
+	
+	  if (statsCount < NUM_FPS) { // obtain the average FPS and UPS
+	    averageFPS = totalFPS/statsCount;
+	    averageUPS = totalUPS/statsCount;
+	  }
+	  else {
+	    averageFPS = totalFPS/NUM_FPS;
+	    averageUPS = totalUPS/NUM_FPS;
+	  }
+	/*
+	 System.out.println(
+	   timedf.format( (double) statsInterval/1000000000L) + " " +
+	   timedf.format((double) realElapsedTime/1000000000L)+"s "+
+	   df.format(timingError) + "% " +
+	   frameCount + "c " +
+	   framesSkipped + "/" + totalFramesSkipped + " skip; " +
+	   df.format(actualFPS) + " " + df.format(averageFPS)+" afps; " +
+	   df.format(actualUPS) + " " + df.format(averageUPS)+" aups" );
+	*/
+	  framesSkipped = 0;
+	  prevStatsTime = timeNow;
+	  statsInterval = 0L;   // reset
+	}
+  }
   
   //actively render the buffer image to the screen
   private void paintScreen(){
@@ -234,7 +354,7 @@ public class Ciclo extends JPanel implements Runnable {
 
 
   private void gameUpdate() { 
-    if (!gameOver){
+	  if (!isPaused && !gameOver) {
     	// update game state ...
 	}
   }  
@@ -257,8 +377,17 @@ public class Ciclo extends JPanel implements Runnable {
     dbg.fillRect (0, 0, PWIDTH, PHEIGHT);
 
     // draw game elements
-    // ...
+    dbg.setColor(Color.blue);
+    dbg.setFont(font);
+    
 
+    //  report average FPS and UPS at top left
+    dbg.drawString("Average FPS/UPS: " + df.format(averageFPS) +
+             ", " + df.format(averageUPS), 20, 25);
+
+    dbg.setColor(Color.black);
+
+    
     if (gameOver)
       gameOverMessage(dbg);
   }
@@ -272,9 +401,19 @@ public class Ciclo extends JPanel implements Runnable {
 
   private void gameOverMessage(Graphics g) { 
 	// code to calculate x and y...
-    g.drawString(msg, x, y);
+    g.drawString(gameOverString, 20, 20);
   } 
 
-  
+  public void resumeGame()
+  // called when the JFrame is activated / deiconified
+  {  isPaused = false;  }
+
+  public void pauseGame()
+  // called when the JFrame is deactivated / iconified
+  { isPaused = true;   }
+
+  public void stopGame()
+  // called when the JFrame is closing
+  {  running = false;   }  
   
 }
